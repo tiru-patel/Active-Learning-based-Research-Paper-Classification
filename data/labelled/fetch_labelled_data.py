6b2259a9888dfdabdbd19f909e38a3051519be42
Jaskaran Singh <jaskaransingh@Jaskarans-MacBook-Pro.local>
2022-12-10 14:05:21 -0500

    Adding new datasets

--- data/labelled/fetch_labelled_data.py
+++ data/labelled/fetch_labelled_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import json
 
+from sklearn.model_selection import train_test_split
+
 # Update filename
 def decode_json(filename, save_csv=False):
     with open(filename) as f:
@@ -21,9 +23,26 @@ def get_CS_data(df_final, save_csv=False):
     df_filtered.reset_index(inplace=True)
     df_filtered = df_filtered.drop(columns=['index'], axis=1)
     if save_csv==True:
-        df_filtered.to_csv('labelled_data.csv')
+        df_filtered.to_csv('cs_data.csv')
     return df_filtered
 
+def get_active_learning_data(data, dataset_ratio = 0.2, save_csv_labelled = False, save_csv_unlabelled=False):
+    df_labelled, df_unlabelled = train_test_split(data, train_size = dataset_ratio, stratify=data['categories'], random_state=42)
+
+    if save_csv_labelled == True:
+        df_labelled.to_csv('labelled_data.csv')
+
+    if save_csv_unlabelled == True:
+        df_unlabelled.to_csv('../unlabelled_data/unlabelled_data_json.csv')
+
+    return df_labelled, df_unlabelled
+
 if __name__== "__main__":
-    main_df = decode_json(save_csv=True)
-    labelled_data = get_CS_data(main_df, save_csv=True)
+    main_df = decode_json('arxiv-metadata-oai-snapshot.json',save_csv=True)
+    cs_data = get_CS_data(main_df, save_csv=True)
+
+    labelled_data, unlabelled_data = get_active_learning_data(cs_data, 0.2, True, True)
+
+
+
+
