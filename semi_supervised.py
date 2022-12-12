d656bb29dcb48eee7f037d36637c7b7791209624
Jaskaran Singh <jaskaransingh@Jaskarans-MacBook-Pro.local>
2022-12-12 02:36:29 -0500

    Finalizing the semi-supervised script

--- semi_supervised.py
+++ semi_supervised.py
@@ -9,7 +9,8 @@ from sklearn.semi_supervised import LabelPropagation
 from sklearn.semi_supervised import SelfTrainingClassifier
 from sklearn.model_selection import train_test_split as tts
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+from imblearn.over_sampling import BorderlineSMOTE
+from sklearn.metrics import accuracy_score
 
 
 class Semisupervised():
@@ -30,23 +31,31 @@ class Semisupervised():
         y = df["categories"]
 
         train_x,test_x,train_y,test_y = tts(x,y,test_size=0.2,stratify=y)
+        self.test_labels = test_y
 
-        tfidf_vect = TfidfVectorizer(stop_words = 'english')
+        tfidf_vect = TfidfVectorizer(stop_words = 'english', max_features=1000)
         
         tfidf_train = tfidf_vect.fit_transform(train_x)
         self.tfidf_test = tfidf_vect.transform(test_x)
-        
+
+        sm2 = BorderlineSMOTE(random_state=42)
+        tfidf_train, train_y = sm2.fit_resample(tfidf_train, train_y)
+
+        tfidf_unlab = tfidf_vect.transform(unlab_x)
+
+        tfidf_train_df = pd.DataFrame(tfidf_train.A, columns=tfidf_vect.get_feature_names_out())
+        tfidf_unlab_df = pd.DataFrame(tfidf_unlab.A, columns=tfidf_vect.get_feature_names_out())
+
         le = LabelEncoder()
         self.encoded_y = le.fit_transform(train_y)
         self.test_encoded_y = le.fit_transform(test_y)
 
-        self.X_train_text = np.concatenate((np.array(train_x), np.array(unlab_x)))
+        self.X_train_text = tfidf_train_df.append(tfidf_unlab_df, ignore_index=True)
 
         nolabel = [-1 for _ in range(len(unlab_x))]
         self.y_train_final = np.concatenate((self.encoded_y, nolabel))
 
-        tfidf_semi_sup = tfidf_vect.transform(self.X_train_text)
-        self.X_train_final = pd.DataFrame(tfidf_semi_sup.A, columns=tfidf_vect.get_feature_names_out())
+        self.X_train_final = pd.DataFrame(self.X_train_text, columns=tfidf_vect.get_feature_names_out())
 
     def train_label_propagation(self):
         # define model
@@ -59,8 +68,9 @@ class Semisupervised():
     
     def predict_label_propagation(self, model):
         pred = model.predict(self.tfidf_test)
+        acc = accuracy_score(self.test_encoded_y, pred)
 
-        return pred
+        return pred, acc
 
     def train_self_training_classifier(self, base_model = LogisticRegression(solver='lbfgs', max_iter=1000)):
         # Specify Self-Training model parameters
@@ -73,8 +83,9 @@ class Semisupervised():
 
     def predict_self_training_classifier(self, model):
         pred = model.predict(self.tfidf_test)
+        acc = accuracy_score(self.test_encoded_y, pred)
 
-        return pred
+        return pred, acc
 
 
 
